5670d591d3ad2bfc141ff071e4a8c55a710809e5
vkb-lab <vkb@kaizen-org.com>
2026-03-28 17:32:16 -0300

    NAVE-MAE: Remote Agent Auto-Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,1 @@
-import './globals.css'
-
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="pt-br">
-      <body>{children}</body>
-    </html>
-  )
-}
+import './globals.css'; export default function RootLayout({children}: {children: React.ReactNode}) { return (<html lang='pt-br'><body>{children}</body></html>); }
