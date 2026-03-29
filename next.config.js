60fd69421aa15cf35b25fa8148257ba3e7c5f369
vkb-lab <vkb@kaizen-org.com>
2026-03-29 09:00:10 -0300

    KAIZEN: Stable 14.2.15 Build - Fixed Types

--- next.config.js
+++ next.config.js
@@ -1,6 +1,1 @@
-
-module.exports = {
-  typescript: { ignoreBuildErrors: true },
-  eslint: { ignoreDuringBuilds: true },
-  swcMinify: true
-}
+module.exports = { typescript: { ignoreBuildErrors: true }, eslint: { ignoreDuringBuilds: true } }
